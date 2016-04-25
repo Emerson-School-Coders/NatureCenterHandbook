af9c20489f4101d99a3c54a435bb26dc9c1d6fae
MCJack123 <jackbruienne@yahoo.com>
2016-04-25 17:43:45 -0400

    Update login_handler.php

--- login_handler.php
+++ login_handler.php
@@ -1,7 +1,4 @@
 <?php
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
 /*$conn = new mysqli("localhost", "root");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
