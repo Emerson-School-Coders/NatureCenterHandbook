e11bad194e4a69e08dc1ec63980e784d481f10b8
MCJack123 <jackbruienne@yahoo.com>
2016-04-25 10:41:13 -0400

    Added files via upload

---
+++ upload.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>The Emerson Nature Center Official Handbook | Upload</title>
+</head>
+<body>
+<form onSubmit="var text = document.forms[0].elements[0].value; text = text.replace(/\r?\n/g, '<br />'); document.write('This will be reviewed: ' + text)">
+<textarea width=300 height=300>Text</textarea>
+<input type="submit" value="submit">
+</form>
+<br>
+<form onSubmit="document.write('Image Uploaded!')">
+Image 1 <input type="file"> Insert image with [img 1] <input type="submit" value="submit">
+</form>
+</body>
+</html>
