8f9520c11a92ec189ab2b0765dfcfa11c1ddec6e
MCJack123 <jackbruienne@yahoo.com>
2016-04-25 18:28:40 -0400

    Update login_handler.php

--- login_handler.php
+++ login_handler.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "natcenter");
+$conn = new mysqli("localhost", "root", "7isthebest");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
