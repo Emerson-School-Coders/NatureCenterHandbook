ed0d1f5aa448afd009be4eb11ab85ea76373c0d8
MCJack123 <jackbruienne@yahoo.com>
2016-04-25 16:57:12 -0400

    Update login_handler.php

--- login_handler.php
+++ login_handler.php
@@ -1,5 +1,8 @@
 <html>
-<head>
-<title>Logging you into the Emerson Nature Center Handbook...</title>
-</head>
+  <head>
+    <title>Logging you into the Emerson Nature Center Handbook...</title>
+  </head>
+  <body>
+    Password: <?php echo $_POST["password"]; ?>
+  </body>
 </html>
