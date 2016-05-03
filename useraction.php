10ba78be1b1b6de845fb222c2d39b065f430eb30
MCJack123 <jackbruienne@yahoo.com>
2016-05-03 13:55:04 -0400

    Update useraction.php

--- useraction.php
+++ useraction.php
@@ -6,10 +6,16 @@ if ($_POST["action"] == "adduser") {
   if (!isset($_POST["candelete"])) {$_POST["candelete"] = "0";}
   $db->exec("INSERT INTO passwords (id,password,canadd,canedit,candelete) VALUES (NULL,\"" . $_POST["password"] . "\"," . $_POST["canadd"] . "," . $_POST["canedit"] . "," . $_POST["candelete"] . ")");
 }
+if(isset($_REQUEST["destination"])){
+      header("Location: {$_REQUEST["destination"]}");
+  }else if(isset($_SERVER["HTTP_REFERER"])){
+      header("Location: {$_SERVER["HTTP_REFERER"]}");
+  }else{
+       header("Location: /index.html");
+  }
 ?>
 <html>
   <head>
     <title>Doing user action...</title>
-    <script>history.go(-1);</script>
   </head>
 </html>
