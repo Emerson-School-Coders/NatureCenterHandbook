0b725de2d44b497ed0f8dd1098cdffcde3202150
MCJack123 <jackbruienne@yahoo.com>
2016-05-03 15:54:38 -0400

    Update control_panel.php

--- control_panel.php
+++ control_panel.php
@@ -37,6 +37,7 @@ else {$tim = false;}
         <?php if (!$modpass) {echo 'ID: <input type="text" name="password"><br><input type="hidden" name="modpass" value="true"><input type="submit" value="Continue"><!-- ';} ?>
         <input type="hidden" name="destination" value="<?php echo $_SERVER['REQUEST_URL']; ?>">
         <input type="hidden" name="action" value="moduser">
+        ID: <?php echo $passid; ?>
         <input type="hidden" name="id" value="<?php echo $passid; ?>">
         Can add pages: <input type="checkbox" name="canadd" value="1" <?php if ($modpass) {if ($db->querySingle("SELECT canadd FROM passwords WHERE id = " . $passid) == "1") {echo "checked";}}?>><br>
         Can edit pages: <input type="checkbox" name="canedit" value="1" <?php if ($modpass) {if ($db->querySingle("SELECT canedit FROM passwords WHERE id = " . $passid) == "1") {echo "checked";}}?>><br>
