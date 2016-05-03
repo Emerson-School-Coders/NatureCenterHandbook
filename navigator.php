81efa230e1ca761fee4d92fb70a21da47a0b480a
MCJack123 <jackbruienne@yahoo.com>
2016-05-03 19:11:35 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -11,5 +11,5 @@ h3.navbar, a.navbar {
 }
 </style>
 <div class="navbar">
-<h3 class="navbar" align="center"><a class="navbar" href="index.html">Home</a> | <a class="navbar" href="upload.php">Upload</a> | <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a> | Logged in as ID: ' . $_COOKIE['userid'] . ''; ?><?php include_once "sql.inc"; if (isset($_COOKIE['istim'])) echo ' | <a href="control_panel.php" class="navbar">Control Panel</a>'; ?></h3>
+<h3 class="navbar" align="center"><a class="navbar" href="index.html">Home</a> | <a class="navbar" href="upload.php">Upload</a> | <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a> | Logged in as: ID ' . $_COOKIE['userid'] . ''; ?><?php include_once "sql.inc"; if (isset($_COOKIE['istim'])) echo ' | <a href="control_panel.php" class="navbar">Control Panel</a>'; ?></h3>
 </div>
