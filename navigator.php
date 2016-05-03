2041eda87966de2d0f3073e1d2bb575528313946
MCJack123 <jackbruienne@yahoo.com>
2016-05-03 18:39:41 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -11,5 +11,5 @@ h3.navbar, a.navbar {
 }
 </style>
 <div class="navbar">
-<h3 class="navbar" align="center"><a class="navbar" href="index.html">Home</a> | <a class="navbar" href="upload.php">Upload</a> | <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.html">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a>'; ?><?php if (!isset($db)) include("sql.inc"); if ($db->querySingle("SELECT istim FROM passwords WHERE id=".$_COOKIE['userid']) == 1) {echo ' | <a href="control_panel.php" class="navbar">Control Panel</a>';} ?></h3>
+<h3 class="navbar" align="center"><a class="navbar" href="index.html">Home</a> | <a class="navbar" href="upload.php">Upload</a> | <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.html">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a>'; ?><?php if ($db->querySingle("SELECT istim FROM passwords WHERE id=".$_COOKIE['userid']) == 1) {echo ' | <a href="control_panel.php" class="navbar">Control Panel</a>';} ?></h3>
 </div>
