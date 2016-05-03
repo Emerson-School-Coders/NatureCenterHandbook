d61f9da7fc23707f9cc243dc1a9e14ffa63cb499
MCJack123 <jackbruienne@yahoo.com>
2016-05-03 16:37:45 -0400

    Update useraction.php

--- useraction.php
+++ useraction.php
@@ -15,6 +15,12 @@ else if ($_POST["action"] == "moduser") {
   if (!isset($_POST["candelete"])) {$_POST["candelete"] = "0";}
   $db->exec("UPDATE passwords SET canadd=".$_POST["canadd"].", canedit=".$_POST["canedit"].", candelete=".$_POST["candelete"]." WHERE id=".$_POST["id"]);
 }
+else if ($_POST["action"] == "changepass") {
+  $oldpass = $db->querySingle("SELECT password FROM passwords WHERE istim=1");
+  if ($_POST['oldpass'] == $oldpass) {
+    $db->exec("UPDATE passwords SET password=".$_POST['newpass']." WHERE istim=1");
+  }
+}
 if(isset($_REQUEST["destination"])){
       header("Location: {$_SERVER["HTTP_REFERER"]}");
   }else if(isset($_SERVER["HTTP_REFERER"])) {
