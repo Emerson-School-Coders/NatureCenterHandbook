dc83db5d99682163db6a0f1ef31a62cb427bd0d3
MCJack123 <jackbruienne@yahoo.com>
2016-05-03 13:44:35 -0400

    Update useraction.php

--- useraction.php
+++ useraction.php
@@ -1,6 +1,9 @@
 <?php
 include("sql.inc");
 if ($_POST["action"] == "adduser") {
+  if (!isset($_POST["canadd"])) {$_POST["canadd"] = "0";}
+  if (!isset($_POST["canedit"])) {$_POST["canedit"] = "0";}
+  if (!isset($_POST["candelete"])) {$_POST["candelete"] = "0";}
   $db->exec("INSERT INTO passwords (id,password,canadd,canedit,candelete) VALUES (NULL,\"" . $_POST["password"] . "\"," . $_POST["canadd"] . "," . $_POST["canedit"] . "," . $_POST["candelete"] . ")");
 }
 ?>
