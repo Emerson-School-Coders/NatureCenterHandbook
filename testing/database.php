d8087eb8130367b905bc41db0a2745b36faf8b71
MCJack123 <jackbruienne@yahoo.com>
2016-05-06 10:36:21 -0400

    Update and rename database.php to testing/database.php

--- testing/database.php
+++ testing/database.php
@@ -1,5 +1,10 @@
 <?php
-include "sql.inc";
+class MyDB extends SQLite3 {
+  function __construct() {
+    $this->open('../database.db');
+  }
+}
+$db = new MyDB();
 $i = 1;
 while ($query = $db->querySingle("SELECT * FROM passwords WHERE id=" . $i, true)) {
   print_r($query);
