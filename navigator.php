f6b45800abffa1aa2e4cff72ac58abb45d2294b9
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 18:36:09 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -11,10 +11,14 @@ h3.navbar, a.navbar {
 }
 </style>
 <div class="navbar">
-<h3 class="navbar" align="center"><a class="navbar" href="index.html">Home</a> | 
-<a class="navbar" href="upload.php">Upload</a> | <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php">Login</a>'; 
-else echo '<a class="navbar" href="login_handler.php?logout">Logout</a> | Logged in as: ID ' . $_COOKIE['userid'] . ''; ?>
-<?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo ' | <a href="control_panel.php" class="navbar">Control Panel</a>'; ?>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<form action="view.php" method="GET">Search: <input type="search" name="search"> <input type="submit" value="Search"></form></h3>
+  <ul>
+    <h3 class="navbar" align="center">
+      <li><a class="navbar" href="index.html">Home</a> | </li>
+      <li><a class="navbar" href="upload.php">Upload</a> | </li>
+      <li><?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a> | Logged in as: ID ' . $_COOKIE['userid'] . ''; ?></li>
+      <?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo '<li> | <a href="control_panel.php" class="navbar">Control Panel</a></li>'; ?>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <li><form action="view.php" method="GET">Search: <input type="search" name="search"> <input type="submit" value="Search"></form></li>
+    </h3>
+  </ul>
 </div>
