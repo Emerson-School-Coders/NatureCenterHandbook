81229b0614660ea04fe897146e5f94a79d371e32
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 18:54:56 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -17,15 +17,14 @@ h3.navbar, a.navbar {
 </head>
 <body>
 <div class="navbar">
-  
     <h3 class="navbar" align="center">
       <a class="navbar" href="index.html">Home</a> | 
       <a class="navbar" href="upload.php">Upload</a> | 
       <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a> | Logged in as: ID ' . $_COOKIE['userid'] . ''; ?>
       <?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo ' | <a href="control_panel.php" class="navbar">Control Panel</a>'; ?>
       </h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <form action="view.php" method="GET"><h3 class="navbar" align="center">Search: <input type="search" name="search"></h3><input type="submit" value="Search"></form></li>
-  
+      <form action="view.php" method="GET">Search: <input type="search" name="search"><input type="submit" value="Search"></form></li>
+    </h3>
 </div>
 </body>
 </html>
