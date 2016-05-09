4483678187240ebe173d361ec672c949d106458e
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 19:28:30 -0400

    Update view.php

--- view.php
+++ view.php
@@ -1,3 +1,16 @@
+<?php
+//Get the function of the page
+if ($_SERVER['QUERY_STRING'] == "")     $pmode = 0; // show search page
+if (isset($_GET['search']))             $pmode = 1; // show search results
+else                                    $pmode = 2; // show entry
+?>
 <html>
 <head>
-<script>document.write("<title>The Emerson Nature Center Official Handbook | ");</script>
+  <script>var pmode = <?php echo $pmode; ?></script>
+  <script>
+    document.write("<title>The Emerson Nature Center Official Handbook | ");
+    if (pmode == 0) document.write("Search the Handbook");
+    else if (pmode == 1) document.write("Search Results");
+    else document.write("<?php echo $db->querySingle("SELECT name FROM handbook WHERE id=" . $_GET['id']); ?>");
+    document.write("</title>");
+  </script>
