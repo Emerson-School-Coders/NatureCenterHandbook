93a3073d8a8f81d67ddb46fe4b5b25883246943f
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 18:48:55 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -25,9 +25,8 @@ ul li {
       <li><a class="navbar" href="upload.php">Upload</a> | </li>
       <li><?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout">Logout</a> | Logged in as: ID ' . $_COOKIE['userid'] . ''; ?></li>
       <?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo '<li> | <a href="control_panel.php" class="navbar">Control Panel</a></li>'; ?>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <li><form action="view.php" method="GET"><h3 class="navbar">Search: <input type="search" name="search"></h3><input type="submit" value="Search"></form></li>
-    </h3>
+      </h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <li><form action="view.php" method="GET"><h3 class="navbar">Search: <input type="search" name="search"></h3><input type="submit" value="Search"></form></li>
   </ul>
 </div>
 </body>
