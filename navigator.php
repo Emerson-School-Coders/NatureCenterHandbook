6f786e61f2d4305bd1715e677176aeec26688ed0
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 19:13:44 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -20,7 +20,8 @@ form {
 <body>
 <div class="navbar">
     <h3 class="navbar" align="center">
-      <a class="navbar" href="index.html" target="_top">Home</a> | 
+      <a class="navbar" href="index.php" target="_top">Home</a> | 
+      <a class="navbar" href="view.php" target="_top">View</a> | 
       <a class="navbar" href="upload.php" target="_top">Upload</a> | 
       <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php" target="_top">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout" target="_top">Logout</a>'; ?>
       <?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo ' | <a href="control_panel.php" class="navbar" target="_top">Control Panel</a>'; ?>
