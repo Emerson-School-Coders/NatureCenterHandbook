59471be52b1cf4ef7643f131eec22f9312ff0dec
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 21:30:40 -0400

    Update view.php

--- view.php
+++ view.php
@@ -1,8 +1,13 @@
 <?php
+include "sql.inc"
 //Get the function of the page
 if ($_SERVER['QUERY_STRING'] == "" || !isset($_SERVER['QUERY_STRING'])) $pmode = 0; // show search page
 else if (isset($_GET['search'])) $pmode = 1; // show search results
 else $pmode = 2; // show entry
+if ($_SERVER['QUERY_STRING'] == "init") {
+  $db->exec("CREATE TABLE handbook (id INT PRIMARY KEY, name STRING, title STRING, author STRING, entry STRING, imageids STRING)");
+  $db->exec('INSERT INTO handbook VALUES (NULL, "Sample Entry", "Sample Handbook Entry", "John Doe", "This is an example entry to test the Handbook out. This will be removed with the public release of the Handbook.", "1,2")');
+}
 ?>
 <html>
   <head>
@@ -18,17 +23,16 @@ else $pmode = 2; // show entry
   </head>
   <body>
     <iframe src="navigator.php"></iframe>
-    <?php if ($pmode != 0) {
-      echo '<!--';
-    }
-    ?>
+    <?php if ($pmode != 0) echo '<!--'; ?>
     <h1>Search the Nature Center Handbook</h1>
     <form action="view.php" method="GET">
       <p>Search: <input type="search" name="search" placeholder="Search the Handbook..."><input type="submit" value="Search"></p>
     </form>
-    <?php if ($pmode != 0) {
-      echo '-->';
-    }
-    ?>
+    <?php if ($pmode != 0) echo '-->';
+    if ($pmode != 1) echo '<!--'; ?>
+    <h1>Search Results</h1>
+    <ul>
+      
+    </ul>
   </body>
 </html>
