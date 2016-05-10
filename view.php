5cad9f0e09c7514d53775613809d4ba02ce32d11
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 21:13:27 -0400

    Update view.php

--- view.php
+++ view.php
@@ -3,6 +3,7 @@
 if ($_SERVER['QUERY_STRING'] == "") $pmode = 0; // show search page
 if (isset($_GET['search'])) $pmode = 1; // show search results
 else $pmode = 2; // show entry
+echo $pmode;
 ?>
 <html>
   <head>
