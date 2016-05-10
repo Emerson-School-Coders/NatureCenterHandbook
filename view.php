0d4ef3faa4407a3769fd76f344bc3a7f711371a1
MCJack123 <jackbruienne@yahoo.com>
2016-05-10 15:42:22 -0400

    Update view.php

--- view.php
+++ view.php
@@ -3,8 +3,9 @@ include "sql.inc";
 //Get the function of the page
 if ($_SERVER['QUERY_STRING'] == "" || !isset($_SERVER['QUERY_STRING'])) $pmode = 0; // show search page
 else if (isset($_GET['search'])) $pmode = 1; // show search results
+else if ($_SERVER['QUERY_STRING'] == "init") $pmode = 3;
 else $pmode = 2; // show entry
-if ($_SERVER['QUERY_STRING'] == "init") {
+if ($pmode == 3) {
   $db->exec("CREATE TABLE handbook (id INT PRIMARY KEY, name STRING, title STRING, author STRING, entry STRING, imageids STRING)");
   $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry", "Sample Handbook Entry", "John Doe", "This is an example entry to test the Handbook out. This will be removed with the public release of the Handbook.", "1,2")');
   $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry2", "Sample Handbook Entry #2", "John Doe", "This is another example entry to test the Handbooks multiple choice selection out. This will be removed with the public release of the Handbook.", "3,4")');
@@ -24,6 +25,7 @@ if ($_SERVER['QUERY_STRING'] == "init") {
   </head>
   <body>
     <iframe src="navigator.php"></iframe>
+    <?php if ($pmode == 3) echo "Initialized.";
     <?php if ($pmode != 0) echo '<!--'; ?>
     <h1>Search the Nature Center Handbook</h1>
     <form action="view.php" method="GET">
