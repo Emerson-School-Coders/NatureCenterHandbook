53f4c216f74dff0aa4e003a73edf87c1e47fb9ab
MCJack123 <jackbruienne@yahoo.com>
2016-05-10 15:47:05 -0400

    Update view.php

--- view.php
+++ view.php
@@ -39,7 +39,7 @@ if ($pmode == 3) {
       if ($pmode == 1) {
         $results = $db->querySingle("SELECT id FROM handbook WHERE title LIKE '%".$search."%' UNION SELECT id FROM handbook WHERE entry LIKE '%".$search."%'", true);
         foreach ($results as $result) {
-          echo '<li><a href="view.php?id='.$result.'">'.$db->querySingle("SELECT title FROM handbook WHERE id=".$result."</a></li>");
+          echo '<li><a href="view.php?id='.$result.'">' . $db->querySingle("SELECT title FROM handbook WHERE id=".$result) . '</a></li>';
         }
       }
       ?>
