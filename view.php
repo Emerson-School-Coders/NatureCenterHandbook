0048575091b27f5155d0775f420cd0edd47ac10b
MCJack123 <jackbruienne@yahoo.com>
2016-05-09 21:07:08 -0400

    Update view.php

--- view.php
+++ view.php
@@ -5,12 +5,30 @@ if (isset($_GET['search']))             $pmode = 1; // show search results
 else                                    $pmode = 2; // show entry
 ?>
 <html>
-<head>
-  <script>var pmode = <?php echo $pmode; ?></script>
-  <script>
-    document.write("<title>The Emerson Nature Center Official Handbook | ");
-    if (pmode == 0) document.write("Search the Handbook");
-    else if (pmode == 1) document.write("Search Results");
-    else document.write("<?php echo $db->querySingle("SELECT name FROM handbook WHERE id=" . $_GET['id']); ?>");
-    document.write("</title>");
-  </script>
+  <head>
+    <script>var pmode = <?php echo $pmode; ?></script>
+    <script>
+      document.write("<title>The Emerson Nature Center Official Handbook | ");
+      if (pmode == 0) document.write("Search the Handbook");
+      else if (pmode == 1) document.write("Search Results");
+      else document.write("<?php echo $db->querySingle("SELECT name FROM handbook WHERE id=" . $_GET['id']); ?>");
+      document.write("</title>");
+    </script>
+    <link rel="stylesheet" href="page.css" type="text/css">
+  </head>
+  <body>
+    <iframe src="navigator.php"></iframe>
+    <?php if ($pmode != 0) {
+      echo '<!--';
+    }
+    ?>
+    <h1>Search the Nature Center Handbook</h1>
+    <form action="view.php" method="GET">
+      <p>Search: </p><input type="search" name="search" placeholder="Search the Handbook..."><input type="submit" value="Search">
+    </form>
+    <?php if ($pmode != 0) {
+      echo '-->';
+    }
+    ?>
+  </body>
+</html>
