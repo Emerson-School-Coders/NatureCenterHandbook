a43a88e19c99fd83ae835b83144b88b3ed3a7bf7
MCJack123 <jackbruienne@yahoo.com>
2016-05-11 18:59:56 -0400

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -1,5 +1,73 @@
 <?php
-
+include "sql.inc";
+$target_dir = "images/";
+$images = scandir($target_dir);
+$last_id = ltrim(end($images), "id-");
+$last_id = rtrim($last_id, ".png");
+$firstid = intval($last_id) + 1;
+$target_file = $target_dir . "id-" . $firstid . "." . pathinfo($_FILES["image1"]["name"], PATHINFO_EXTENSION);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+$check = getimagesize($_FILES["image1"]["tmp_name"]);
+if ($check !== false) {
+} else {
+  echo "The first file you uploaded is not a valid PNG image.";
+  $uploadOk = 0;
+}
+if ($_FILES["image1"]["size"] > 500000) {
+  echo "Sorry, your first file is too large.";
+  $uploadOk = 0;
+}
+if ($imageFileType != "png"){
+  echo 'You can only upload PNG images. (first file)';
+  $uploadOk = 0;
+}
+if ($uploadOk == 0) {
+  echo "Sorry, your first file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["image1"]["tmp_name"], $target_file)) {
+    $uploadOk = 1;
+  } else {
+     echo "Sorry, there was an error uploading your first file.";
+  }
+}
+if (isset($_FILES['image2'])) {
+  $target_dir = "images/";
+$images = scandir($target_dir);
+$last_id = ltrim(end($images), "id-");
+$last_id = rtrim($last_id, ".png");
+$secondid = $firstid + 1;
+$target_file = $target_dir . "id-" . $secondid . "." . pathinfo($_FILES["image2"]["name"], PATHINFO_EXTENSION);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+$check = getimagesize($_FILES["image2"]["tmp_name"]);
+if ($check !== false) {
+} else {
+  echo "The second file you uploaded is not a valid PNG image.";
+  $uploadOk = 0;
+}
+if ($_FILES["image2"]["size"] > 500000) {
+  echo "Sorry, your second file is too large.";
+  $uploadOk = 0;
+}
+if ($imageFileType != "png"){
+  echo 'You can only upload PNG images. (second file)';
+  $uploadOk = 0;
+}
+if ($uploadOk == 0) {
+  echo "Sorry, your second file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["image2"]["tmp_name"], $target_file)) {
+    $uploadOk = 1;
+  } else {
+     echo "Sorry, there was an error uploading your second file.";
+  }
+}
+} else $secondid = "\b";
+$db->exec('INSERT INTO handbook (id,title,author,entry,imageids) VALUES (NULL,"'.$_POST["title"].'","'.$_POST["author"].'","'.$_POST["entry"].'","'.$firstid.','.$secondid.'")');
+header("Location: view.php?id=".$db->querySingle("SELECT id FROM handbook WHERE title=".$_POST['title']));
 ?>
 <html>
   <head>
