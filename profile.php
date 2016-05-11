c471ee861b4d46d475aa57536fce359815687681
MCJack123 <jackbruienne@yahoo.com>
2016-05-11 15:10:18 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -16,7 +16,7 @@
         echo "<li>No entries.</li>";
       } else {
         while ($post) {
-          echo '<li><a href="view.php?id='.$db->querySingle('SELECT id FROM handbook WHERE title="'.$post.'"').'">'.$post.'</a></li>'
+          echo '<li><a href="view.php?id='.$db->querySingle('SELECT id FROM handbook WHERE title="'.$post.'"').'">'.$post.'</a></li>';
           $post = $posts->fetchArray();
         }
       }
