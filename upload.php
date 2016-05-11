2e8e76559f8f08e3080fa01c1b9de94953e84989
MCJack123 <jackbruienne@yahoo.com>
2016-05-11 11:04:49 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,31 +1,28 @@
 <html>
-<head>
-<title>The Emerson Nature Center Official Handbook | Upload</title>
-<link rel="stylesheet" href="page.css" type="text/css">
-<iframe src="navigator.php"></iframe>
-<style>
-textarea#styled {
-	width: 600px;
-	height: 120px;
-	border: 3px solid #cccccc;
-	padding: 5px;
-}
-</style>
-</head>
-<body>
-<br>
-<center>
-<form onSubmit="var text = document.forms[0].elements[0].value; text = text.replace(/\r?\n/g, '<br />'); document.write('This will be reviewed: ' + text)">
-<textarea id="styled" width="300" height="300" placeholder="text"></textarea>
-<br>
-<input type="submit" value="submit">
-</form>
-<br>
-<form onSubmit="document.write('Image Uploaded!')">
-Image 1 <input type="file">
-<br>
-Insert image with [img 1] <input type="submit" value="submit">
-</form>
-</center>
-</body>
+	<head>
+		<title>The Emerson Nature Center Official Handbook | Upload</title>
+		<link rel="stylesheet" href="page.css" type="text/css">
+		<iframe src="navigator.php"></iframe>
+		<style>
+			textarea#styled {
+				width: 600px;
+				height: 120px;
+				border: 3px solid #cccccc;
+				padding: 5px;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Upload an entry to the Handbook!</h1>
+		<center>
+			<form action="upload_page.php" method="POST">
+				<h1 style="entry">Title: <input type="text" name="title" placeholder="Title" style="height: 100px; font-size: 24pt;"></h1><br>
+				<h3 style="entry">Author: <?php echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?><br>
+				<textarea id="styled" width="300" height="300" placeholder="text"></textarea>
+				<br>
+				<input type="submit" value="submit">
+			</form>
+			<br>
+		</center>
+	</body>
 </html>
