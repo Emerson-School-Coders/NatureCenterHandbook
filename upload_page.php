dcfee7f09fef2514ce9edc8235632c3d2b28c705
MCJack123 <jackbruienne@yahoo.com>
2016-05-11 19:14:23 -0400

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -16,11 +16,11 @@ $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 //  $uploadOk = 0;
 //}
 if ($_FILES["image1"]["size"] > 500000) {
-  echo "Sorry, your first file is too large.";
+  echo "Sorry, your first file is too large. Your size was " . $_FILES["image1"]["size"] . " bytes.";
   $uploadOk = 0;
 }
 if ($imageFileType != "png"){
-  echo 'You can only upload PNG images. (first file)';
+  echo 'You can only upload PNG images. (first file) Your type was ' . $imageFileType;
   $uploadOk = 0;
 }
 if ($uploadOk == 0) {
@@ -42,18 +42,18 @@ $secondid = $firstid + 1;
 $target_file = $target_dir . "id-" . $secondid . "." . pathinfo($_FILES["image2"]["name"], PATHINFO_EXTENSION);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-$check = getimagesize($_FILES["image2"]["tmp_name"]);
-if ($check !== false) {
-} else {
-  echo "The second file you uploaded is not a valid PNG image.";
-  $uploadOk = 0;
-}
+//$check = getimagesize($_FILES["image2"]["tmp_name"]);
+//if ($check !== false) {
+//} else {
+//  echo "The second file you uploaded is not a valid PNG image.";
+//  $uploadOk = 0;
+//}
 if ($_FILES["image2"]["size"] > 500000) {
-  echo "Sorry, your second file is too large.";
+  echo "Sorry, your second file is too large. Your size was " . $_FILES["image1"]["size"] . " bytes.";
   $uploadOk = 0;
 }
 if ($imageFileType != "png"){
-  echo 'You can only upload PNG images. (second file)';
+  echo 'You can only upload PNG images. (second file) Your type was ' . $imageFileType;
   $uploadOk = 0;
 }
 if ($uploadOk == 0) {
@@ -68,7 +68,8 @@ if ($uploadOk == 0) {
 }
 } else $secondid = "\b";
 $db->exec('INSERT INTO handbook (id,title,author,entry,imageids) VALUES (NULL,"'.$_POST["title"].'","'.$_POST["author"].'","'.$_POST["entry"].'","'.$firstid.','.$secondid.'")');
-header("Location: view.php?id=".$db->querySingle("SELECT id FROM handbook WHERE title='".$_POST['title']."'"));
+if (!headers_sent()) header("Location: view.php?id=".$db->querySingle("SELECT id FROM handbook WHERE title='".$_POST['title']."'"));
+else echo 'Please press the back button on your browser.';
 ?>
 <html>
   <head>
