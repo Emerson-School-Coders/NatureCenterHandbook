b035e46d53f7373ed892509e687cb12767d50269
MCJack123 <jackbruienne@yahoo.com>
2016-05-12 18:33:34 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -18,7 +18,7 @@
 		<center>
 			<?php if (!isset($_COOKIE['userid'])) echo '<p>Sorry, uploading is only available to registered accounts. Please ask Tim for an account with uploading permissions.</p><!--';
 			else if (!$canadd = $db->querySingle("SELECT canadd FROM passwords WHERE id=".$_COOKIE['userid'])) echo "<p>Sorry, you are not allowed to upload new entries. Please ask Tim for permission to add pages.</p><!--";?>
-			<form action="upload_page.php" method="POST">
+			<form action="upload_page.php" method="POST" enctype="multipart/form-data">
 				<h2>Title: <input type="text" name="title" placeholder="Title" style="font-size: 18pt;" required></h1>
 				<input type="hidden" name="author" value="<?php if ($canadd) echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?>">
 				<h3 style="entry">Author: <?php if ($canadd) echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?>
