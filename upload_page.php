539f6c3d9b4c3978e5eb67d0b253ac8a4f48e23d
MCJack123 <jackbruienne@yahoo.com>
2016-05-13 11:05:20 -0400

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -35,32 +35,32 @@ if ($uploadOk == 0) {
      echo "Sorry, there was an error uploading your first file.<br>";
   }
 }
+$uploadOks = 1;
 if (isset($_FILES['image2']['name']) && !empty($_FILES['image2']['name'])) {
   $target_dir = "images/";
 $secondid = $firstid + 1;
 $target_file = $target_dir . "id-" . $secondid . ".png";
-$uploadOk = 1;
 $check = getimagesize($_FILES["image2"]["tmp_name"]);
 if ($check !== false) {
-  $uploadOk = 1;
+  $uploadOks = 1;
 } else {
   echo "The second file you uploaded is not a valid PNG image.<br>";
-  $uploadOk = 0;
+  $uploadOks = 0;
 }
 if ($_FILES["image2"]["size"] > 500000) {
   echo "Sorry, your second file is too large. Your size was " . $_FILES["image2"]["size"] . " bytes.<br>";
-  $uploadOk = 0;
+  $uploadOks = 0;
 }
 if (pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "png"){
   echo 'You can only upload PNG images. (second file) Your type was ' . $_FILES['image2']['type'] . "<br>";
-  $uploadOk = 0;
+  $uploadOks = 0;
 }
-if ($uploadOk == 0) {
+if ($uploadOks == 0) {
   echo "Sorry, your second file was not uploaded.<br>";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["image2"]["tmp_name"], $target_file)) {
-    $uploadOk = 1;
+    $uploadOks = 1;
   } else {
      echo "Sorry, there was an error uploading your second file.<br>";
   }
@@ -68,8 +68,8 @@ if ($uploadOk == 0) {
 }
 if (isset($secondid)) $secondid = ",".$secondid;
 else $secondid = "";
-if ($uploadOk == 1) $db->exec('INSERT INTO handbook (id,title,author,entry,imageids) VALUES (NULL,"'.$_POST["title"].'","'.$_POST["author"].'","'.$_POST["entry"].'","'.$firstid.','.$secondid.'")');
-else echo "Your entry was not added.<br>";
+if ($uploadOk == 1 && $uploadOks == 1) $db->exec('INSERT INTO handbook (id,title,author,entry,imageids) VALUES (NULL,"'.$_POST["title"].'","'.$_POST["author"].'","'.$_POST["entry"].'","'.$firstid.','.$secondid.'")');
+else {echo "Your entry was not added.<br>"; header(""); flush();}
 if (!headers_sent()) header("Location: view.php?id=".$db->querySingle("SELECT id FROM handbook WHERE title='".$_POST['title']."'"));
 else echo 'Please press the back button on your browser.';
 ?>
