c5fd877064d3e61de9969ce67840617e62566942
ThorThunderIsCoder <twhelgeson@gmail.com>
2016-05-19 11:01:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ document.write("<font color='red'>PHP is not installed!  That may cause some pro
 <h1>Welcome to the Emerson Nature Center's Official Handbook!</h1>
 <p>This is a handbook for students and by students to help eduacate people on the plants found in the Nature Center! There are already many different descriptions here, but if you would like to add your own descriptions, you can submit a request by clicking the "upload" button. Tim will review the entry, and it will be posted as soon as possible (if approved)! Please feel free to explore using the table of contents below. </p>
 <left><img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Grass_covered_house_in_Iceland_1972.jpg" height="200"/></left>
-<center><top><img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" height="300"/></top</center>
+<center><top><img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" height="300"/></top></center>
 
 <?php include 'cp.php'; ?>
 </body>
