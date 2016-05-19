f3aeb8f4f8601aa3308bf2623005c431476e4a78
ealgase <mostdigitsofpi@gmail.com>
2016-05-19 08:35:23 -0400

    Add licensing and about page

--- navigator.php
+++ navigator.php
@@ -22,7 +22,8 @@ form {
     <h3 class="navbar" align="center">
       <a class="navbar" href="index.php" target="_top">Home</a> | 
       <a class="navbar" href="view.php" target="_top">View</a> | 
-      <a class="navbar" href="upload.php" target="_top">Upload</a> | 
+      <a class="navbar" href="upload.php" target="_top">Upload</a> |
+      <a class="navbar" href="about.php" target="_top">About Us</a> |  
       <?php if (!isset($_COOKIE['userid'])) echo '<a class="navbar" href="login.php" target="_top">Login</a>'; else echo '<a class="navbar" href="login_handler.php?logout" target="_top">Logout</a>'; ?>
       <?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo ' | <a href="control_panel.php" class="navbar" target="_top">Control Panel</a>'; ?>
       &nbsp;&nbsp;&nbsp;
