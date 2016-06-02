7ec20598682f996aa5f76d5c1a041435aebbef66
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 11:45:14 -0400

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -20,25 +20,25 @@ $check = getimagesize($_FILES["image1"]["tmp_name"]);
 if ($check !== false) {
   $uploadOk = 1;
 } else {
- echo "The first file you uploaded is not a valid PNG image.<br>";
+ echof( "The first file you uploaded is not a valid PNG image.<br>");
   $uploadOk = 0;
 }
 if ($_FILES["image1"]["size"] > 500000) {
-  echo "Sorry, your first file is too large. Your size was " . $_FILES["image1"]["size"] . " bytes.<br>";
+  echof( "Sorry, your first file is too large. Your size was " . $_FILES["image1"]["size"] . " bytes.<br>");
   $uploadOk = 0;
 }
 if (pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "png") {
-  echo 'You can only upload PNG images. (first file) Your type was "' . $_FILES['image1']['type'] . '".<br>';
+  echof( 'You can only upload PNG images. (first file) Your type was "' . $_FILES['image1']['type'] . '".<br>');
   $uploadOk = 0;
 }
 if ($uploadOk == 0) {
-  echo "Sorry, your first file was not uploaded.<br>";
+  echof( "Sorry, your first file was not uploaded.<br>");
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["image1"]["tmp_name"], $target_file)) {
     $uploadOk = 1;
   } else {
-     echo "Sorry, there was an error uploading your first file.<br>";
+     echof( "Sorry, there was an error uploading your first file.<br>");
      $uploadOk = 0;
   }
 }
@@ -51,25 +51,25 @@ $check = getimagesize($_FILES["image2"]["tmp_name"]);
 if ($check !== false) {
   $uploadOks = 1;
 } else {
-  echo "The second file you uploaded is not a valid PNG image.<br>";
+  echof( "The second file you uploaded is not a valid PNG image.<br>");
   $uploadOks = 0;
 }
 if ($_FILES["image2"]["size"] > 500000) {
-  echo "Sorry, your second file is too large. Your size was " . $_FILES["image2"]["size"] . " bytes.<br>";
+  echof( "Sorry, your second file is too large. Your size was " . $_FILES["image2"]["size"] . " bytes.<br>");
   $uploadOks = 0;
 }
 if (pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "png"){
-  echo 'You can only upload PNG images. (second file) Your type was ' . $_FILES['image2']['type'] . "<br>";
+  echof( 'You can only upload PNG images. (second file) Your type was ' . $_FILES['image2']['type'] . "<br>");
   $uploadOks = 0;
 }
 if ($uploadOks == 0) {
-  echo "Sorry, your second file was not uploaded.<br>";
+  echof( "Sorry, your second file was not uploaded.<br>");
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["image2"]["tmp_name"], $target_file)) {
     $uploadOks = 1;
   } else {
-     echo "Sorry, there was an error uploading your second file.<br>";
+     echof( "Sorry, there was an error uploading your second file.<br>");
      $uploadOk = 0;
   }
 }
@@ -77,7 +77,7 @@ if ($uploadOks == 0) {
 if (isset($secondid)) $secondid = ",".$secondid;
 else $secondid = "";
 if ($uploadOk == 1 && $uploadOks == 1) $db->exec('INSERT INTO handbook (id,title,author,entry,imageids) VALUES (NULL,"'.$_POST["title"].'","'.$_POST["author"].'","'.$_POST["entry"].'","'.$firstid.','.$secondid.'")');
-else {echo "Your entry was not added.<br>"; header(""); flush();}
+else {echo $echof . "Your entry was not added.<br>"; header(""); flush();}
 if (!headers_sent()) header("Location: view.php?id=".$db->querySingle("SELECT id FROM handbook WHERE title='".$_POST['title']."'"));
 else echo 'Please press the back button on your browser.';
 ?>
