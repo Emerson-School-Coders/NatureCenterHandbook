a5f0bf397272486514e4e2618bea150729753e2c
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 11:16:55 -0400

    Update setcookie.php

--- testing/setcookie.php
+++ testing/setcookie.php
@@ -5,6 +5,7 @@ $_COOKIE[$_POST['cookie_name']] = $_POST['cookie_value'];
 <body>
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
 <input type="text" name="cookie_name"> = <input type="text" name="cookie_value">
+<input type="submit" value="submit">
 </form>
 </body>
 </html>
