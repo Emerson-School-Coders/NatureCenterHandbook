25b76a9d31a85f121be863b7159ca339fbcb3593
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 14:18:55 -0400

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -71,9 +71,7 @@ if ($uploadOks == 0) {
      $uploadOk = 0;
   }
 }
-}
-if (isset($secondid)) $secondid = ",".$secondid;
-else $secondid = "";
+} else $secondid = -1;
 if ($uploadOk == 1 && $uploadOks == 1 && $echof == ":") $insert_result = $db->exec('INSERT INTO handbook (id,title,author,entry,imageids) VALUES (NULL,"'.$_POST["title"].'","'.$_POST["author"].'","'.$_POST["entry"].'","'.$firstid.",".$secondid.'")');
 else {echo $echof . "Your entry was not added.<br>Upload 1: ".$uploadOk."Upload 2: ".$uploadOks."Echo: ".$echof; header(""); flush();}
 if (!$insert_result) {die("An error occurred inserting the entry."); unlink($target_file); if (isset($_FILES['image2']['name']) && !empty($_FILES['image2']['name'])) unlink($target_files);}
