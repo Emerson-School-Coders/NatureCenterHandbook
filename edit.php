2d9bf715f1a3baace1712e71a677c5473dcd14fd
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 22:18:20 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -2,6 +2,7 @@
 include "sql.inc";
 if (!isset($_GET['id'])) die("Please enter an ID to edit.");
 $page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WHERE id=".$_GET['id'], true);
+$hidden = false;
 ?>
 <html>
   <head>
@@ -11,8 +12,8 @@ $page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WH
   <body>
     <iframe src="navigator.php"></iframe>
     <h1>Edit Page</h1>
-    <?php if (!isset($_COOKIE['userid'])) echo "<p>You have to log in to the Handbook to edit pages.</p><!--";
-    else if (!$db->querySingle("SELECT canedit FROM passwords WHERE id=".$_COOKIE['userid'])) echo "<p>Sorry, you are not allowed to edit this page. Please ask Tim for permission.</p><!--"; ?>
+    <?php if (!isset($_COOKIE['userid'])) {$hidden = true; echo "<p>You have to log in to the Handbook to edit pages.</p><!--";}
+    else if (!$db->querySingle("SELECT canedit FROM passwords WHERE id=".$_COOKIE['userid'])) {$hidden = true; echo "<p>Sorry, you are not allowed to edit this page. Please ask Tim for permission.</p><!--";} ?>
     <center>
     <form action="edit_page.php" method="POST">
       <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
@@ -27,6 +28,15 @@ $page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WH
               echo '<br>Image 2: <img src="images/id-'.substr($page['imageids'], 2, 1).'.png" height=100> Change: <input type="file" name="image2" accept=".png, image/png">'; ?>
       <br><input type="submit" value="Submit">
     </form>
+    <?php if (!$db->querySingle("SELECT candelete FROM passwords WHERE id=".$_COOKIE['userid'])) {$hidden = true; echo "<!--";} ?>
+    <form action="edit_page.php" method="GET" onSubmit="return alert('Are you sure you want to delete this?');">
+      <h3>Delete this page</h3>
+      <p style="color: red;">WARNING: The only way to recover this entry is to ask Tim to restore it. It is otherwise unrecoverable.</p>
+      <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+      <input type="hidden" name="delete" value="yes">
+      <input type="submit" value="Remove" style="color: red">
+    </form>
     </center>
+    <?php if ($hidden) echo "-->"; ?>
   </body>
 </html>
