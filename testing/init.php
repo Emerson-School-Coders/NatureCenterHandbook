85ce32862e334cb5a3a022a39032982a8ab25a77
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 21:35:13 -0400

    Update init.php

--- testing/init.php
+++ testing/init.php
@@ -13,9 +13,13 @@ if (isset($_GET['action']) || $_GET['action'] == "") {
   }
   else if ($_GET['action'] == "handbook") {
     $db->exec("DROP TABLE handbook");
-    $db->exec("CREATE TABLE handbook (id INTEGER PRIMARY KEY, name STRING, title STRING, author STRING, entry STRING, imageids STRING)");
-    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry", "Sample Handbook Entry", "John Doe", "This is an example entry to test the Handbook out. This will be removed with the public release of the Handbook.", "1,2")');
-    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry2", "Sample Handbook Entry #2", "John Doe", "This is another example entry to test the Handbooks multiple choice selection out. This will be removed with the public release of the Handbook.", "3,4")');
+    $db->exec("CREATE TABLE handbook (id INTEGER PRIMARY KEY, name STRING, title STRING, author STRING, entry STRING, imageids STRING, lasteditor STRING)");
+    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry", "Sample Handbook Entry", "John Doe", "This is an example entry to test the Handbook out. This will be removed with the public release of the Handbook.", "1,2", "")');
+    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry2", "Sample Handbook Entry #2", "John Doe", "This is another example entry to test the Handbooks multiple choice selection out. This will be removed with the public release of the Handbook.", "3,4", "")');
+  }
+  else if ($_GET['action'] == "deleted") {
+    $db->exec("DROP TABLE deleted");
+    $db->exec("CREATE TABLE deleted (id INTEGER, title STRING, author STRING, entry STRING, removerid INTEGER, removaldate INTEGER)");
   }
 }
 ?>
@@ -28,5 +32,6 @@ if (isset($_GET['action']) || $_GET['action'] == "") {
     ?>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET"><input type="hidden" name="action" value="passwords"><input type="submit" value="Initialize Passwords"></form>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET"><input type="hidden" name="action" value="handbook"><input type="submit" value="Initialize Handbook"></form>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET"><input type="hidden" name="action" value="deleted"><input type="submit" value="Initialize Deleted Entries"></form>
   </body>
 </html>
