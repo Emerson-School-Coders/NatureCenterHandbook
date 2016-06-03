6f19d32736e480a0fbfb5bbb8c90b93ad5767208
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 21:05:22 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,14 +4,6 @@
 		<title>The Emerson Nature Center Official Handbook | Upload</title>
 		<link rel="stylesheet" href="page.css" type="text/css">
 		<iframe src="navigator.php"></iframe>
-		<style>
-			textarea#styled {
-				width: 600px;
-				height: 120px;
-				border: 3px solid #cccccc;
-				padding: 5px;
-			}
-		</style>
 	</head>
 	<body>
 		<h1>Upload an entry to the Handbook!</h1>
