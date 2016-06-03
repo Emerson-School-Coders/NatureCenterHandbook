4e23e67cfa3ecfaba2b1c89ed0c04ccb5d28a643
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 21:02:02 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -9,6 +9,7 @@ $page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WH
     <link rel="stylesheet" href="page.css" type="text/css">
   </head>
   <body>
+    <iframe src="navigator.php"></iframe>
     <h1>Edit Page</h1>
     <?php if (!isset($_COOKIE['userid'])) echo "<p>You have to log in to the Handbook to edit pages.</p><!--";
     else if (!$db->querySingle("SELECT canedit FROM passwords WHERE id=".$_COOKIE['userid'])) echo "<p>Sorry, you are not allowed to edit this page. Please ask Tim for permission.</p><!--"; ?>
@@ -20,7 +21,9 @@ $page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WH
       Change images here:
       </h3>
       Image 1: <img src="<?php echo 'images/id-'.substr($page['imageids'], 0, 1).'.png'; ?>"> Change: <input type="file" name="image1" accept=".png, image/png">
-      <?php if (array_slice(explode(",", $page["imageids"]), 1, 1))
+      <?php if (array_slice(explode(",", $page["imageids"]), 1, 1) != "-1" || array_slice(explode(",", $page["imageids"]), 1, 1) != "")
+              echo 'Image 2: <img src="images/id-'.substr($page['imageids'], 0, 1).'.png"> Change: <input type="file" name="image2" accept=".png, image/png">'; ?>
+      <input type="submit" value="Submit">
     </form>
     </center>
   </body>
