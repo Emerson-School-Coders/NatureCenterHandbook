8001aa598a6ae1b4c315bae19e5ffc80f7f6b7ca
MCJack123 <jackbruienne@yahoo.com>
2016-06-02 20:50:14 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,3 +1,27 @@
+<?php
+include "sql.inc";
+if (!isset($_GET['id'])) die("Please enter an ID to edit.");
+$page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WHERE id=".$_GET['id'], true);
+?>
 <html>
-  
+  <head>
+    <title>The Emerson Nature Center Official Handbook - Edit Page</title>
+    <link rel="stylesheet" href="page.css" type="text/css">
+  </head>
+  <body>
+    <h1>Edit Page</h1>
+    <?php if (!isset($_COOKIE['userid'])) echo "<p>You have to log in to the Handbook to edit pages.</p><!--";
+    else if (!$db->querySingle("SELECT canedit FROM passwords WHERE id=".$_COOKIE['userid'])) echo "<p>Sorry, you are not allowed to edit this page. Please ask Tim for permission.</p><!--"; ?>
+    <center>
+    <form action="upload_page.php" method="POST">
+      <h2>Title: <input type="text" name="title" value="<?php echo $page['title']; ?>" placeholder="Title" style="font-size: 18pt;" required></h2>
+      <h3>Author: <?php echo $page['author']; ?>, edited by <?php echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?><br>
+      <textarea id="styled" width=300 height=300 placeholder="Enter your entry here." name="entry" required><?php echo $page['entry']; ?></textarea>
+      Change images here:
+      </h3>
+      Image 1: <img src="<?php echo 'images/id-'.substr($page['imageids'], 0, 1).'.png'; ?>"> Change: <input type="file" name="image1" accept=".png, image/png">
+      <?php if (array_slice(explode(",", $page["imageids"]), 1, 1))
+    </form>
+    </center>
+  </body>
 </html>
