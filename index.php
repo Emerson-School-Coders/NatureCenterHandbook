62d00eb5bf42748f0c8dacf32cb255bbd710bd15
MCJack123 <jackbruienne@yahoo.com>
2016-06-06 21:49:18 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title>The Emerson Nature Center Official Handbook - Home</title>
 <link rel="stylesheet" href="page.css" type="text/css">
-<iframe src="navigator.php"></iframe>
+<iframe src="navigator.php?header=Welcome%20t%20the%20Emerson%20Nature%20Center%27s%20Official%20Handbook%21"></iframe>
 </head>
 <body>
 <script>
@@ -10,7 +10,6 @@ if(!("<?='hello';?>"=="hello")){
 document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
 }
 </script><!--check if php is on the server-->
-<h1>Welcome to the Emerson Nature Center's Official Handbook!</h1>
 <p>This is a handbook for students and by students to help eduacate people on the plants found in the Nature Center! There are already many different descriptions here, but if you would like to add your own descriptions, you can submit a request by clicking the "upload" button. Tim will review the entry, and it will be posted as soon as possible (if approved)! Please feel free to explore using the "view" page. </p>
 <center>
 <table>
