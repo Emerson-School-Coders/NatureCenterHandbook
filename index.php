b8ed927581bb3db1ec0bd4fa9439d32d0663cdcc
MCJack123 <jackbruienne@yahoo.com>
2016-06-07 12:37:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,15 +11,6 @@ document.write("<font color='red'>PHP is not installed!  That may cause some pro
 }
 </script><!--check if php is on the server-->
 <p>This is a handbook for students and by students to help eduacate people on the plants found in the Nature Center! There are already many different descriptions here, but if you would like to add your own descriptions, you can submit a request by clicking the "upload" button. Tim will review the entry, and it will be posted as soon as possible (if approved)! Please feel free to explore using the "view" page. </p>
-<center>
-<table>
-  <tr>
-    <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" height="300"/></td>
-    <td><img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Grass_covered_house_in_Iceland_1972.jpg" height="200"/></td>
-    <td><a href="https://www.google.com/search?q=nature&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjOjtbLzInNAhUJT1IKHRR4AVwQ_AUIBygB&biw=1242&bih=585"><img src="https://static.pexels.com/photos/909/flowers-garden-colorful-colourful.jpg" height="300"</td></a>
-  </tr>
-</table>
-</center>
 <?php include 'cp.php'; ?>
 </body>
 </html>
