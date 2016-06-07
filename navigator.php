26d4b5cbb2b9b05265ca263f76e6cdd5658a826d
MCJack123 <jackbruienne@yahoo.com>
2016-06-07 12:17:58 -0400

    Update navigator.php

--- navigator.php
+++ navigator.php
@@ -80,7 +80,7 @@ window.onclick = function(event) {
     <h3 class="navbar" align="center">
       <a class="navbar" href="index.php" target="_top">Home</a> | 
       <a class="navbar" href="view.php" target="_top">View</a> | 
-      <div class="dropdown"><button class="dropbtn" onClick="hoverbar('profile-drop-menu')">Profiles</button><div id="profile-drop-menu" class="dropdown-content"><?php if (!isset($_COOKIE['userid'])) echo '<a href="login.php" target="_top">Login</a>'; else echo '<a href="login_handler.php?logout" target="_top">Logout</a>'; ?> | <a href="usersearch.php" target="_top">Search for user</a><?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo ' | <a href="control_panel.php" target="_top">Control Panel</a>'; ?></div></div>
+      <h3 style="color: green;"><div class="dropdown"><button class="dropbtn" onClick="hoverbar('profile-drop-menu')">Profiles</button><div id="profile-drop-menu" class="dropdown-content"><?php if (!isset($_COOKIE['userid'])) echo '<a href="login.php" target="_top">Login</a>'; else echo '<a href="login_handler.php?logout" target="_top">Logout</a>'; ?> | <a href="usersearch.php" target="_top">Search for user</a><?php include_once "sql.inc"; if (isset($_COOKIE['tim'])) echo ' | <a href="control_panel.php" target="_top">Control Panel</a>'; ?></div></div></h3>
       &nbsp;&nbsp;&nbsp;
       Search: <form action="view.php" method="GET" target="_top" class="navbar"><input type="search" name="search" placeholder="Search the Handbook..."><input type="submit" value="Search"></form></li>
     </h3>
