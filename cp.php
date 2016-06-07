e72d0abb5bb0712d2bccf9fbc60dc42129961e3d
MCJack123 <jackbruienne@yahoo.com>
2016-06-07 13:16:39 -0400

    Update cp.php

--- cp.php
+++ cp.php
@@ -1,1 +1,1 @@
-<p><center>&copy;&nbsp;<?php echo date("Y"); ?>&nbsp;Emerson School Coders&nbsp;&nbsp;&nbsp;<a href="about.php">About this Project</a>&nbsp;&nbsp;&nbsp;<a href="how_to_write.php">How to Write an Entry</center></p>
+<p style="color: #066418;font-family: 'Trebuchet MS';"><center>&copy;&nbsp;<?php echo date("Y"); ?>&nbsp;Emerson School Coders&nbsp;&nbsp;&nbsp;<a href="about.php">About this Project</a>&nbsp;&nbsp;&nbsp;<a href="how_to_write.php">How to Write an Entry</center></p>
