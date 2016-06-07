5abc34721cc4b4eeadd1ab3aa1a0ff663d183f67
MCJack123 <jackbruienne@yahoo.com>
2016-06-07 13:23:10 -0400

    Update cp.php

--- cp.php
+++ cp.php
@@ -1,7 +1,5 @@
-<p style="color: #066418; font-family: 'Trebuchet MS';">
-  <center style="color: #066418; font-family: 'Trebuchet MS';">
-    &copy;&nbsp;<?php echo date("Y"); ?>&nbsp;Emerson School Coders&nbsp;&nbsp;&nbsp;
-    <a href="about.php">About this Project</a>&nbsp;&nbsp;&nbsp;
-    <a href="how_to_write.php">How to Write an Entry</a>
-  </center>
-</p>
+<center style="color: #066418; font-family: 'Trebuchet MS';">
+  &copy;&nbsp;<?php echo date("Y"); ?>&nbsp;Emerson School Coders&nbsp;&nbsp;&nbsp;
+  <a href="about.php">About this Project</a>&nbsp;&nbsp;&nbsp;
+  <a href="how_to_write.php">How to Write an Entry</a>
+</center>
