fa824d6cd61c45543d790cf9de9cfe63eca8e2ae
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 11:12:12 -0400

    Update sendpdf.php

--- sendpdf.php
+++ sendpdf.php
@@ -1,3 +1,13 @@
 <?php
-
+include "sql.inc";
+$pdfout = new FPDF();
+$handbook = $db->query("SELECT title, author, entry, imageids FROM handbook");
+while ($entry = $handbook->FetchArray()) {
+$pdfout->AddPage("P", "Letter");
+$pdfout->SetFont("Arial", "", 14);
+$pdfout->Text(100, 100, $entry["title"]);
+$pdfout->Text(150, 250, $entry["author"]);
+$pdfout->Write(200, 400, $entry["entry"]);
+}
+$pdf->Output("I");
 ?>
