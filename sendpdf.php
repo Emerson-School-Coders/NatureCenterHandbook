2873ccc487816507109bb1fb6af26d99b217de63
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 20:29:07 -0400

    Update sendpdf.php

--- sendpdf.php
+++ sendpdf.php
@@ -9,9 +9,9 @@ $pdfout->SetTitle("Nature Center Handbook PDF");
 while ($entry = $handbook->FetchArray()) {
 $pdfout->AddPage("P", "Letter");
 $pdfout->SetFont("Helvetica", "B", 24);
-$pdfout->Text(($pdfout->GetPageHeight() - $pdfout->GetStringWidth($entry["title"])) / 2.7, 25, $entry["title"]);
+$pdfout->Text(($pdfout->GetPageWidth() - $pdfout->GetStringWidth($entry["title"])) / 2, 25, $entry["title"]);
 $pdfout->SetFont("Helvetica", "I", 16);
-$pdfout->Text(($pdfout->GetPageHeight() - $pdfout->GetStringWidth($entry["author"])) / 2.7, 50, $entry["author"]);
+$pdfout->Text(($pdfout->GetPageWidth() - $pdfout->GetStringWidth($entry["author"])) / 2, 50, $entry["author"]);
 $pdfout->SetFont("Helvetica", "", 12);
 $pdfout->SetXY(10, 85);
 $pdfout->Write(6, $entry["entry"]);
