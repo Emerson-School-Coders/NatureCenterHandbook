19b7d6314e9f9835093cb90f32ad22b4a6b3b777
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 20:56:14 -0400

    Update init.php

--- testing/init.php
+++ testing/init.php
@@ -14,8 +14,8 @@ if (isset($_GET['action']) || $_GET['action'] == "") {
   else if ($_GET['action'] == "handbook") {
     $db->exec("DROP TABLE handbook");
     $db->exec("CREATE TABLE handbook (id INTEGER PRIMARY KEY, name STRING, title STRING, author STRING, entry STRING, imageids STRING, lasteditor STRING)");
-    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry", "Sample Handbook Entry", "John Doe", "This is an example entry to test the Handbook out. This will be removed with the public release of the Handbook.", "1,2", "")');
-    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry2", "Sample Handbook Entry #2", "John Doe", "This is another example entry to test the Handbooks multiple choice selection out. This will be removed with the public release of the Handbook.", "3,4", "")');
+    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry", "Sample Handbook Entry", "John Doe", "This is an example entry to test the Handbook out. This will be removed with the public release of the Handbook.", "1.png,2.png", "")');
+    $db->exec('INSERT INTO handbook VALUES (NULL, "SampleEntry2", "Sample Handbook Entry #2", "John Doe", "This is another example entry to test the Handbooks multiple choice selection out. This will be removed with the public release of the Handbook.", "3.png,4.png", "")');
   }
   else if ($_GET['action'] == "deleted") {
     $db->exec("DROP TABLE deleted");
