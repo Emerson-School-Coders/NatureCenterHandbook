afb5d8f8ffb2150f36bfa661edee37e3407fabc9
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 21:05:41 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -24,7 +24,7 @@ $hidden = false;
       <br>Change images here:
       </h3>
       Image 1: <img src="<?php echo 'images/id-'.substr($page['imageids'], 0, 1); ?>" height=100> Change: <input type="file" name="image1" accept=".png, image/png, .jpg, image/jpg, .gif, image/gif">
-      <?php if (explode(",", $page["imageids"])[1] != "-1" && explode(",", $page["imageids"])[1] != "")
+      <?php if (explode(",", $page["imageids"])[2] != "-1" && explode(",", $page["imageids"])[2] != "")
               echo '<br>Image 2: <img src="images/id-'.substr($page['imageids'], 2, 1).'" height=100> Change: <input type="file" name="image2" accept=".png, image/png, .jpg, image/jpg, .gif, image/gif">'; ?>
       <br><input type="submit" value="Submit">
     </form>
