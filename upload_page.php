52778e96f8fafb87f213ecde9ea912d02a0066a9
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 21:00:55 -0400

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -13,22 +13,22 @@ $images = scandir($target_dir);
 $last_id = ltrim(end($images), "id-");
 $last_id = rtrim($last_id, ".png");
 $firstid = intval($last_id) + 1;
-$target_file = $target_dir . "id-" . $firstid . ".png";
+$target_file = $target_dir . "id-" . $firstid . "." . pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION);
 $uploadOk = 1;
 $typeAllowed = array("image/png");
 $check = getimagesize($_FILES["image1"]["tmp_name"]);
 if ($check !== false) {
   $uploadOk = 1;
 } else {
- echof( "The first file you uploaded is not a valid PNG image.<br>");
+ echof( "The first file you uploaded is not a valid image.<br>");
   $uploadOk = 0;
 }
 if ($_FILES["image1"]["size"] > 500000) {
   echof( "Sorry, your first file is too large. Your size was " . $_FILES["image1"]["size"] . " bytes.<br>");
   $uploadOk = 0;
 }
-if (pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "png") {
-  echof( 'You can only upload PNG images. (first file) Your type was "' . $_FILES['image1']['type'] . '".<br>');
+if (pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "png" && pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "jpg" && pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "gif") {
+  echof( 'You can only upload PNG, JPG, or GIF images. (first file) Your type was "' . $_FILES['image1']['type'] . '".<br>');
   $uploadOk = 0;
 }
 if ($uploadOk == 0) {
@@ -46,20 +46,20 @@ $uploadOks = 1;
 if (isset($_FILES['image2']['name']) && !empty($_FILES['image2']['name'])) {
   $target_dir = "images/";
 $secondid = $firstid + 1;
-$target_files = $target_dir . "id-" . $secondid . ".png";
+$target_files = $target_dir . "id-" . $secondid . "." . pathinfo($_FILES['image2']['name'], PATHINFO_EXTENSION);
 $check = getimagesize($_FILES["image2"]["tmp_name"]);
 if ($check !== false) {
   $uploadOks = 1;
 } else {
-  echof( "The second file you uploaded is not a valid PNG image.<br>");
+  echof( "The second file you uploaded is not a valid image.<br>");
   $uploadOks = 0;
 }
 if ($_FILES["image2"]["size"] > 500000) {
   echof( "Sorry, your second file is too large. Your size was " . $_FILES["image2"]["size"] . " bytes.<br>");
   $uploadOks = 0;
 }
-if (pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION) != "png"){
-  echof( 'You can only upload PNG images. (second file) Your type was ' . $_FILES['image2']['type'] . "<br>");
+if (pathinfo($_FILES['image2']['name'], PATHINFO_EXTENSION) != "png" && pathinfo($_FILES['image2']['name'], PATHINFO_EXTENSION) != "jpg" && pathinfo($_FILES['image2']['name'], PATHINFO_EXTENSION) != "gif"){
+  echof( 'You can only upload PNG, JPG, or GIF images. (second file) Your type was ' . $_FILES['image2']['type'] . "<br>");
   $uploadOks = 0;
 }
 if ($uploadOks == 0) {
