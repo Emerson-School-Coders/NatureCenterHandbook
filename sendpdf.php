dab5dc61953a2aaed2e141fa6267a1f1c971fd79
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 20:51:50 -0400

    Update sendpdf.php

--- sendpdf.php
+++ sendpdf.php
@@ -16,7 +16,7 @@ $pdfout->SetFont("Helvetica", "I", 16);
 $pdfout->SetTextColor(136, 136, 136);
 $pdfout->Text(($pdfout->GetPageWidth() - $pdfout->GetStringWidth($entry["author"])) / 2, 60, $entry["author"]);
 $pics = explode(",", $entry["imageids"]); 
-$i = 40;
+$i = 25;
 foreach ($pics as $picid) {
   if ($picid != "-1" && $picid != "") $pdfout->Image("images/id-".$picid.".png", $i += 50, 80, 40, 40);
 }
