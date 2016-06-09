382c92f408e3eea5395d8ca8a9d996b40aef3c16
MCJack123 <jackbruienne@yahoo.com>
2016-06-08 20:22:28 -0400

    Update sendpdf.php

--- sendpdf.php
+++ sendpdf.php
@@ -5,11 +5,14 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $pdfout = new FPDF();
 $handbook = $db->query("SELECT title, author, entry, imageids FROM handbook");
+$pdfout->SetTitle("Nature Center Handbook PDF");
 while ($entry = $handbook->FetchArray()) {
 $pdfout->AddPage("P", "Letter");
-$pdfout->SetFont("Arial", "", 14);
+$pdfout->SetFont("Helvectica", "B", 24);
 $pdfout->Text(($pdfout->GetPageHeight() - $pdfout->GetStringWidth($entry["title"])) / 2.7, 25, $entry["title"]);
+$pdfout->SetFont("Helvectica", "I", 16);
 $pdfout->Text(($pdfout->GetPageHeight() - $pdfout->GetStringWidth($entry["author"])) / 2.7, 50, $entry["author"]);
+$pdfout->SetFont("Helvectica", "", 12);
 $pdfout->SetXY(10, 85);
 $pdfout->Write(12, $entry["entry"]);
 }
