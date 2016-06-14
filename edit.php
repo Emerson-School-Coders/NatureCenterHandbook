446a316aabbf18bd0a9ca0eba5612cf769ce9f9a
MCJack123 <jackbruienne@yahoo.com>
2016-06-13 22:04:05 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php
 include "sql.inc";
 if (!isset($_GET['id'])) die("Please enter an ID to edit.");
-$page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WHERE id=".$_GET['id'], true);
+$page = $db->querySingle("SELECT title, author, entry, imageids FROM handbook WHERE id=".SQLite3::escapeString($_GET['id']), true);
 $hidden = false;
 ?>
 <html>
@@ -13,13 +13,13 @@ $hidden = false;
     <iframe src="navigator.php?header=Edit%20Page"></iframe>
     <!--<h1>Edit Page</h1>-->
     <?php if (!isset($_COOKIE['userid'])) {$hidden = true; echo "<p>You have to log in to the Handbook to edit pages.</p><!--";}
-    else if (!$db->querySingle("SELECT canedit FROM passwords WHERE id=".$_COOKIE['userid'])) {$hidden = true; echo "<p>Sorry, you are not allowed to edit this page. Please ask Tim for permission.</p><!--";} ?>
+    else if (!$db->querySingle("SELECT canedit FROM passwords WHERE id=".SQLite3::escapeString($_COOKIE['userid']))) {$hidden = true; echo "<p>Sorry, you are not allowed to edit this page. Please ask Tim for permission.</p><!--";} ?>
     <center>
     <form action="edit_page.php" method="POST">
       <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
       <input type="hidden" name="author" value="<?php echo $page['author']; ?>">
       <h2>Title: <input type="text" name="title" value="<?php echo $page['title']; ?>" placeholder="Title" style="font-size: 18pt;" required></h2>
-      <h3>Author: <?php echo $page['author']; ?>, edited by <?php echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?><br>
+      <h3>Author: <?php echo $page['author']; ?>, edited by <?php echo $db->querySingle("SELECT name FROM passwords WHERE id=".SQLite3::escapeString($_COOKIE['userid'])); ?><br>
       <textarea id="styled" width=300 height=300 placeholder="Enter your entry here." name="entry" required><?php echo $page['entry']; ?></textarea>
       <br>Change images here:
       </h3>
@@ -28,7 +28,7 @@ $hidden = false;
               echo '<br>Image 2: <img src="images/id-'.substr($page['imageids'], 6, 1).'" height=100> Change: <input type="file" name="image2" accept=".png, image/png, .jpg, image/jpg, .gif, image/gif">'; ?>
       <br><input type="submit" value="Submit">
     </form>
-    <?php if (!$db->querySingle("SELECT candelete FROM passwords WHERE id=".$_COOKIE['userid'])) {$hidden = true; echo "<!--";} ?>
+    <?php if (!$db->querySingle("SELECT candelete FROM passwords WHERE id=".SQLite3::escapeString($_COOKIE['userid']))) {$hidden = true; echo "<!--";} ?>
     <form action="edit_page.php" method="GET" onSubmit="return confirm('Are you sure you want to delete this?');">
       <h3>Delete this page</h3>
       <p style="color: red;">WARNING: The only way to recover this entry is to ask Tim to restore it. It is otherwise unrecoverable.</p>
