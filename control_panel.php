8ca803680a0e7e0f11fde8be8c5313b938482c0a
MCJack123 <jackbruienne@yahoo.com>
2016-09-08 21:43:39 -0400

    Update control_panel.php

--- control_panel.php
+++ control_panel.php
@@ -7,7 +7,7 @@ $modpass = true;
 }
 else {$modpass = false;}
 if (isset($_COOKIE['tim'])) {$tim = true;}
-else {header("HTTP/1.1 403 Forbidden")}
+else {header("HTTP/1.1 403 Forbidden");}
 ?>
 <html>
   <head>
