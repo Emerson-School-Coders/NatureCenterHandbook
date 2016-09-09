29d3f9f7021796b9cd36ef44a1cf42bcc8caba99
MCJack123 <jackbruienne@yahoo.com>
2016-09-08 22:24:22 -0400

    Update control_panel.php

--- control_panel.php
+++ control_panel.php
@@ -91,7 +91,7 @@ else {header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden", true, 403); header(
         <?php
         $deletions = $db->query("SELECT * FROM deleted");
         while ($delete = $deletions->fetchArray(SQLITE3_ASSOC)) {
-          echo '<tr><td>'.$delete['title'].'</td><td>'.$delete['author'].'</td><td>'.$db->querySingle("SELECT name FROM passwords WHERE id=".$delete['removedby']).'</td><td>'.date('Y-m-d', $delete['removaldate']).'</td><td><form action="restore.php" method="POST"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Restore"></td><td><form action="delete.php" method="POST"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Remove" style="color: red" onClick="alert(\'Are you sure you want to delete this forever? It cannot be recovered!\');"></td></tr>';
+          echo '<tr><td>'.$delete['title'].'</td><td>'.$delete['author'].'</td><td>'.$db->querySingle("SELECT name FROM passwords WHERE id=".$delete['removedby']).'</td><td>'.date('Y-m-d', $delete['removaldate']).'</td><td><form action="restore.php" method="POST"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Restore"></td><td><form action="delete.php" method="POST"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Remove" style="color: red" onClick="confirm(\'Are you sure you want to delete this forever? It cannot be recovered!\');"></td></tr>';
         }
         ?>
       </table>
