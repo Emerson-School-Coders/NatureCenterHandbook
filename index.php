e8a5c21e690606b76a8f39954f3b11352625f571
ealgase <mostdigitsofpi@gmail.com>
2016-09-21 19:56:18 -0400

    make some pages more readable(code)

--- index.php
+++ index.php
@@ -1,29 +1,38 @@
+<!DOCTYPE html>
 <html>
-<?php
-include 'sql.inc';
-?>
-<head>
-<title>The Emerson Nature Center Official Handbook - Home</title>
-<link rel="stylesheet" href="page.css" type="text/css">
-<iframe src="navigator.php?header=Welcome%20to%20the%20Handbook%21"></iframe>
-</head>
-<body>
-<script>
-if(!("<?='hello';?>"=="hello")){
-document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
-}
-</script><!--check if php is on the server-->
-<p>This is a handbook for students and by students to help eduacate people on the plants found in the Nature Center! There are already many different descriptions here, but if you would like to add your own descriptions, you can submit a request by clicking the "upload" button. Tim will review the entry, and it will be posted as soon as possible (if approved)! Please feel free to explore using the "view" page. </p>
-<center><h2><font style="font-family: 'Trebuchet MS'; color: green;">Table of Contents</font></h2></center>
-    <ul>
     <?php
-$search = " ";
-$query = 'SELECT id FROM handbook WHERE title LIKE "%'.$search.'%"OR entry LIKE "%'.$search.'%"';
-$results = $db->query($query);
-$result = $results->fetchArray(SQLITE3_NUM);
-while ($result) {echo '<li><a href="view.php?id='.$result[0].'">' . $db->querySingle("SELECT title FROM handbook WHERE id=" . $result[0]) . '</a></li>'; $result = $results->fetchArray(SQLITE3_NUM);}
+        include 'sql.inc';
     ?>
-    </ul>
-<?php include 'cp.php'; ?>
-</body>
+    <head>
+        <title>The Emerson Nature Center Official Handbook - Home</title>
+        <link rel="stylesheet" href="page.css" type="text/css">
+        <iframe src="navigator.php?header=Welcome%20to%20the%20Handbook%21"></iframe>
+    </head>
+    <body>
+        <script>
+            if(!("<?='hello';?>"=="hello")){
+            document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
+            }
+        </script><!--check if php is on the server-->
+        <p>
+            This is a handbook for students and by students to help eduacate people on the plants found in the Nature Center! There are already many different descriptions here, but if you would like to add your own descriptions, you can submit a request by clicking the "upload" button. Tim will review the entry, and it will be posted as soon as possible (if approved)! Please feel free to explore using the "view" page. 
+        </p>
+        <center>
+            <h2>
+                <font style="font-family: 'Trebuchet MS'; color: green;">
+                    Table of Contents
+                </font>
+            </h2>
+        </center>
+            <ul>
+                <?php
+                    $search = " ";
+                    $query = 'SELECT id FROM handbook WHERE title LIKE "%'.$search.'%"OR entry LIKE "%'.$search.'%"';
+                    $results = $db->query($query);
+                    $result = $results->fetchArray(SQLITE3_NUM);
+                    while ($result) {echo '<li><a href="view.php?id='.$result[0].'">' . $db->querySingle("SELECT title FROM handbook WHERE id=" . $result[0]) . '</a></li>'; $result = $results->fetchArray(SQLITE3_NUM);}
+                ?>
+            </ul>
+        <?php include 'cp.php'; ?>
+    </body>
 </html>
--- upload.php
+++ upload.php
@@ -7,13 +7,14 @@
 	</head>
 	<body>
 		<center>
-			<?php if (!isset($_COOKIE['userid'])) echo '<p>Sorry, uploading is only available to registered accounts. Please ask Tim for an account with uploading permissions.</p><img src="NotSignedInError.ico"/><!--';
-			
-			else if (!$canadd = $db->querySingle("SELECT canadd FROM passwords WHERE id=".$_COOKIE['userid'])) echo "<p>Sorry, you are not allowed to upload new entries. Please ask Tim for permission to add pages.</p><!--";?>
+			<?php 
+                if (!isset($_COOKIE['userid'])) echo '<p>Sorry, uploading is only available to registered accounts. Please ask Tim for an account with uploading permissions.</p><img src="NotSignedInError.ico"/><!--';
+                else if (!$canadd = $db->querySingle("SELECT canadd FROM passwords WHERE id=".$_COOKIE['userid'])) echo "<p>Sorry, you are not allowed to upload new entries. Please ask Tim for permission to add pages.</p><!--";
+            ?>
 			<form action="upload_page.php" method="POST" enctype="multipart/form-data">
 				<h2>Title: <input type="text" name="title" placeholder="Title" style="font-size: 18pt;" required></h1>
 				<input type="hidden" name="author" value="<?php if ($canadd) echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?>">
-				<h3 style="entry">Author: <?php if ($canadd) echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?>
+                <h3 style="entry">Author: <?php if ($canadd) echo $db->querySingle("SELECT name FROM passwords WHERE id=".$_COOKIE['userid']); ?></h3>
 				<br>
 				<textarea id="styled" width="300" height="300" placeholder="text" name="entry" required></textarea>
 				<br>
@@ -24,6 +25,6 @@
 			</form>
 			<br>
 		</center>
-<?php include 'cp.php'; ?>
+        <?php include 'cp.php'; ?>
 	</body>
 </html>
