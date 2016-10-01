1443fa093283fc7fb0a6c9f31d4cc0c052f76412
ealgase <mostdigitsofpi@gmail.com>
2016-10-01 13:38:14 -0400

    Update restore.php

--- restore.php
+++ restore.php
@@ -1,4 +1,4 @@
 <?php
   include "sql.inc";
-  print_r($db->querySingle("SELECT * FROM deleted WHERE id=" . $_GET["id"]));
+  print_r($db->querySingle("SELECT * FROM deleted WHERE id=" . $_GET["id"],True));
 ?>
