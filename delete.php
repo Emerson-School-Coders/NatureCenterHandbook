ee70de0281dd06fdf48209e5f219e2e81dad3e5a
MCJack123 <jackbruienne@yahoo.com>
2016-10-01 15:09:12 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 include "sql.inc";
 if (empty($_GET["id"])) header("Location: index.php");
-else $db->exec("DELETE * FROM deleted WHERE id=".$_GET["id"]);
+else $db->exec("DELETE FROM deleted WHERE id=".$_GET["id"]);
 header("Location: control_panel.php");
 ?>
