7b1bc3e76af146898f67b41cae75a9414bbe5bd6
ealgase <mostdigitsofpi@gmail.com>
2016-10-01 14:54:09 -0400

    Update control_panel.php

--- control_panel.php
+++ control_panel.php
@@ -81,7 +81,7 @@ else {header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden", true, 403); header(
     }
     ?>
     </table>
-    </p>
+      </p>
     </div>
     <div id="viewdeletions">
       <h3>View deleted entries</h3>
@@ -90,7 +90,7 @@ else {header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden", true, 403); header(
         <?php
         $deletions = $db->query("SELECT * FROM deleted");
         while ($delete = $deletions->fetchArray(SQLITE3_ASSOC)) {
-          echo '<tr><td>'.$delete['title'].'</td><td>'.$delete['author'].'</td><td>'.$db->querySingle("SELECT name FROM passwords WHERE id=".$delete['removedby']).'</td><td>'.date('Y-m-d', $delete['removaldate']).'</td><td><form action="restore.php" method="POST"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Restore"></td><td><form action="delete.php" method="POST" onsubmit="return confirm(\'Are you sure you want to delete this forever? It cannot be recovered!\');"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Remove" style="color: red"></td></tr>';
+          echo '<tr><td>'.$delete['title'].'</td><td>'.$delete['author'].'</td><td>'.$db->querySingle("SELECT name FROM passwords WHERE id=".$delete['removedby']).'</td><td>'.date('Y-m-d', $delete['removaldate']).'</td><td><form action="restore.php" method="GET"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Restore"></td><td><form action="delete.php" method="GET" onclick="var delete_page = confirm(\'Are you sure you want to delete this forever? It cannot be recovered!\');if(delete_page==true){window.location="delete.php?id=" . $page['id'] . ""}"><input type="hidden" name="id" value="'.$page['id'].'"><input type="submit" value="Remove" style="color: red"></td></tr>';
         }
         ?>
       </table>
