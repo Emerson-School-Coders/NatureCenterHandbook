c1003f1e3e792f4457ded4a538bac32704a8f57a
ealgase <mostdigitsofpi@gmail.com>
2016-10-01 14:39:34 -0400

    Update restore.php

--- restore.php
+++ restore.php
@@ -1,7 +1,7 @@
 <?php
   include "sql.inc";
   $entry = $db->querySingle("SELECT * FROM deleted WHERE id=" . $_GET["id"],True);
-  $db->exec("INSERT INTO handbook (id,title,author,entry) VALUES ($entry['id'],$entry['title'],$entry['author'],$entry['entry'])");
+  $db->exec("INSERT INTO handbook (id,title,author,entry) VALUES (" . $entry['id'] . "," . $entry['title'] . "," . $entry['author'] . "," . $entry['entry'] . ")");
   $db->exec("DELETE FROM deleted WHERE id=" . $_GET["id"]);
   header("Location: control_panel.php");
 ?>
