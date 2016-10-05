09b518ed2a307e093c8427e2b33de24c03d28f16
ealgase <mostdigitsofpi@gmail.com>
2016-10-05 08:32:40 -0400

    Fix some broken syntax

--- how_to_write.php
+++ how_to_write.php
@@ -1,54 +1,63 @@
 <html>
-<head>
-<title>The Emerson Nature Center Official Handbook - Home</title>
-<link rel="stylesheet" href="page.css" type="text/css">
-<iframe src="navigator.php?header=How%20to%20Write%20an%20Entry"></iframe>
-</head>
-<body>
-<script>
-if(!("<?='hello';?>"=="hello")){
-document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
-}
-</script><!--check if php is on the server-->
-<p>This is how you can submit an entry to the Nature Center Handbook.</p>
-<ol>
-<li>
-<br>
-<img src="Upload.png" />
-<br />
-<p>Click Upload</p>
-</li>
-<li>
-<br>
-<img src="UploadPage.png" />
-<br />
-<p>This is the page you go to.  If you get a page saying:
-<br><img src="NotSignedIn.png" /><br>
-you will need to sign in.</p>
-</li>
-<p>
-<li>
-Tim's Expectations:
-<ol>
-<li>
-Name of Plant
-</li>
-<li>
-Where it can be found in the Nature Center
-</li>
-<li>
-Description: leaves, bark(if any), shape, (tree, shrup, weed?)
-</li>
-<li>
-<u>Interesting</u> Info
-</li>
-<li>
-Other
-</li>
-</ol>
-</li>
-</ol>
-</p>
-<?php include 'cp.php'; ?>
-</body>
+  <head>
+    <title>The Emerson Nature Center Official Handbook - Home</title>
+    <link rel="stylesheet" href="page.css" type="text/css">
+    <iframe src="navigator.php?header=How%20to%20Write%20an%20Entry"></iframe>
+  </head>
+  <body>
+    <script>
+      if(!("<?='hello';?>"=="hello")){
+        document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
+      }
+    </script><!--check if php is on the server-->
+    <p>
+      This is how you can submit an entry to the Nature Center Handbook.
+    </p>
+    <ol>
+      <li>
+        <br>
+        <img src="Upload.png" />
+        <br />
+        <p>
+          Click Upload
+        </p>
+      </li>
+      <li>
+        <br>
+        <img src="UploadPage.png" />
+        <br />
+        <p>
+          This is the page you go to.  If you get a page saying:
+          <br>
+          <img src="NotSignedIn.png" />
+          <br>
+          you will need to sign in.
+        </p>
+      </li>
+      <li>
+        Tim's Expectations:
+        <ol>
+          <li>
+            Name of Plant
+          </li>
+          <li>
+            Where it can be found in the Nature Center
+          </li>
+          <li>
+            Description: leaves, bark(if any), shape, (tree, shrup, weed?)
+          </li>
+          <li>
+            <u>
+              Interesting
+            </u>
+            Info
+          </li>
+          <li>
+            Other
+          </li>
+        </ol>
+      </li>
+    </ol>
+    <?php include 'cp.php'; ?>
+  </body>
 </html>
