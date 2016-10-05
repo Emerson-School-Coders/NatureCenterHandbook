d6de09a6194a40a141581315e06e565297953396
ealgase <mostdigitsofpi@gmail.com>
2016-10-05 08:36:01 -0400

    Fix indentation

--- about.php
+++ about.php
@@ -1,24 +1,42 @@
 <html>
-<head>
-<title>The Emerson Nature Center Official Handbook - About</title>
-<link rel="stylesheet" href="page.css" type="text/css">
-<iframe src="navigator.php?header=About%20us"></iframe>
-</head>
-<body>
-<script>
-if(!("<?='hello';?>"=="hello")){
-document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
-}
-</script><!--check if php is on the server-->
-<p>Jack, Elliot, Thor, and Mateo worked on this project as a handbook for the Nature Center. Jacob worked on beta testing the handbook to find bugs. If you want to join, please ask Tim.</p>
-<p><b>Licensing: </b>all text is under the CC BY-SA license.  You are free to copy and distribute the content of this text.</p>
-<p>This project uses the fpdf library to create PDF files. The license is below:<br>
-<code>Permission is hereby granted, free of charge, to any person obtaining a copy<br>
-of this software to use, copy, modify, distribute, sublicense, and/or sell<br>
-copies of the software, and to permit persons to whom the software is furnished<br>
-to do so.<br>
-<br>
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.</code></p>
-<?php include 'cp.php'; ?>
-</body>
+  <head>
+    <title>
+      The Emerson Nature Center Official Handbook - About
+    </title>
+    <link rel="stylesheet" href="page.css" type="text/css">
+    <iframe src="navigator.php?header=About%20us"></iframe>
+  </head>
+  <body>
+    <script>
+      if(!("<?='hello';?>"=="hello")){
+        document.write("<font color='red'>PHP is not installed!  That may cause some problems when viewing this site.  Please contact the system administrator of this site.</font>");
+      }
+    </script><!--check if php is on the server-->
+    <p>
+      Jack, Elliot, Thor, and Mateo worked on this project as a handbook for the Nature Center. Jacob worked on beta testing the handbook to find bugs. If you want to join, please ask Tim.
+    </p>
+    <p>
+      <b>
+        Licensing: 
+      </b>
+      all text is under the 
+      <code>
+        CC BY-SA
+      </code>
+      license.  You are free to copy and distribute the content of this text.
+    </p>
+    <p>
+      This project uses the fpdf library to create PDF files. The license is below:
+      <br>
+    <code>
+      Permission is hereby granted, free of charge, to any person obtaining a copy<br>
+      of this software to use, copy, modify, distribute, sublicense, and/or sell<br>
+      copies of the software, and to permit persons to whom the software is furnished<br>
+      to do so.<br>
+      <br>
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
+      </code>
+    </p>
+    <?php include 'cp.php'; ?>
+  </body>
 </html>
